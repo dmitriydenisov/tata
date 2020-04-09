f7e0ee209111922a7760dcefe970d4873820782d
dmitriydenisov <dmitriyd2009@yandex.ru>
2020-04-09 17:25:40 +0400

    доработал видео в модалке

---
+++ src/js/video.js
@@ -0,0 +1,47 @@
+const tag = document.createElement('script');
+
+tag.src = "https://www.youtube.com/iframe_api";
+const firstScriptTag = document.getElementsByTagName('script')[0];
+firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
+
+let player;
+function onYouTubeIframeAPIReady() {
+  player = new YT.Player('player', {
+    height: '360',
+    width: '640',
+    videoId: 'v98xZn3KGQQ',    
+    events: {
+       //'onReady': onPlayerReady
+    //   'onStateChange': onPlayerStateChange
+    }
+  });
+}
+
+document.querySelector('[data-btn-video]').addEventListener('click', () => {   
+    document.querySelector('[data-modal-video]').style.display='block'
+    //onPlayerReady(event)
+})
+
+function onPlayerReady(event) {
+  event.target.playVideo();
+}
+
+
+document.querySelector('[data-modal-video]').addEventListener('click', () => {
+    document.querySelector('[data-modal-video]').style.display='none' 
+    player.stopVideo();       
+})
+
+document.addEventListener("keydown", function ( event ) {
+    if (event.key === "Escape") {
+        document.querySelector('[data-modal-video]').style.display='none' 
+    }
+})
+
+
+
+//const menuToggle = document.querySelector('#close-video');
+
+// menuToggle.onclick = function(){
+//     menuToggle.classList.toggle('close-video-active');
+// }
